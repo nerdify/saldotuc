0d9a6effdc4962f685a2c5a6a6000068e47f2816
Hosmel Quintana <hosmelq@gmail.com>
2017-06-07 01:31:27 -0600

    make App functional

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from 'views/Home';
 import FourOhFour from 'views/FourOhFour';
+import Home from 'views/Home';
 
 import Header from 'components/Header';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Header />
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
 
-          <Switch>
-            <Route exact component={Home} path="/" />
-            <Route component={FourOhFour} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+        <Switch>
+          <Route exact component={Home} path="/" />
+          <Route component={FourOhFour} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
