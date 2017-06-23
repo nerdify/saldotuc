3940eaaa83ef65d36771828ec54e92a1e56a4234
Hosmel Quintana <hosmelq@gmail.com>
2017-06-23 12:25:35 -0600

    add footer

---
+++ modules/saldotuc-website/src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  font-size: 11px;
+
+  color: #fff;
+  text-align: center;
+
+  a {
+    color: #fff;
+  }
+`;
+
+function Footer() {
+  return (
+    <Wrapper>
+      <p>
+        Handcrafted by <a href="https://getnerdify.com" target="_black">Nerdify</a> in Managua, NI.
+      </p>
+    </Wrapper>
+  );
+}
+
+export default Footer;
--- modules/saldotuc-website/src/views/Home/index.js
+++ modules/saldotuc-website/src/views/Home/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 
+import Footer from 'components/Footer';
 import Icon from 'components/Icon';
 
 import {
@@ -24,27 +25,32 @@ class Home extends PureComponent {
 
   render() {
     return (
-      <Wrapper>
-        <Intro>
-          <Title>Consulta tu saldo<br />sin esfuerzo</Title>
-          <Subtitle>
-            Consultar tus tarjetas
-            <br />
-            <abbr title="Transporte Urbano Colectivo">TUC</abbr>
-            {' '}
-            desde cualquier lugar.
-          </Subtitle>
-          <Buttons>
-            <Button href="https://play.google.com/store/apps/details?id=com.socialimprover.saldotuc">
-              <Icon name="play_store" /> <b>Google</b> play
-            </Button>
-            <Button href="/app">
-              <b>Web</b> app
-            </Button>
-          </Buttons>
-        </Intro>
-        <Image alt="Saldo TUC" src={nexus} />
-      </Wrapper>
+      <div>
+        <Wrapper>
+          <Intro>
+            <Title>Consulta tu saldo<br />sin esfuerzo</Title>
+            <Subtitle>
+              Consultar tus tarjetas
+              <br />
+              <abbr title="Transporte Urbano Colectivo">TUC</abbr>
+              {' '}
+              desde cualquier lugar.
+            </Subtitle>
+            <Buttons>
+              <Button href="https://play.google.com/store/apps/details?id=com.socialimprover.saldotuc">
+                <Icon name="play_store" /> <b>Google</b> play
+              </Button>
+              <Button href="/app">
+                <b>Web</b> app
+              </Button>
+            </Buttons>
+          </Intro>
+          <Image alt="Saldo TUC" src={nexus} />
+
+        </Wrapper>
+
+        <Footer />
+      </div>
     );
   }
 }
